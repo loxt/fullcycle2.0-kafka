8a1c69b2c86dce5c6777158e3526177200921abf
loxt <emannuelloxt@hotmail.com>
2022-04-24 22:14:46 -0400

    feat: add kafka message publisher func

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+)
 
+func main() {
+	producer := NewKafkaProducer()
+	defer producer.Close()
+
+	PublishMessage("aaaaaaaaaaa", "test", producer, nil)
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "fullcycle20-kafka-kafka-1:9092",
+	}
+	producer, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return producer
+}
+
+func PublishMessage(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value: []byte(msg),
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Key: key,
+	}
+	err := producer.Produce(message, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/loxt/fullcycle2.0-kafka
 
 go 1.18
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2
