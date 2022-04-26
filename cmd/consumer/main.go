2b4d1dad1bf6e133cf2b78506c91009d69b6a7cd
loxt <emannuelloxt@hotmail.com>
2022-04-25 21:14:33 -0400

    feat: add kafka consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := kafka.ConfigMap{
+		"bootstrap.servers": "fullcycle20-kafka-kafka-1:9092",
+		"client.id":         "goapp-consumer",
+		"group.id":          "goapp-group",
+	}
+	consumer, error := kafka.NewConsumer(&configMap)
+	if error != nil {
+		fmt.Println("Error creating consumer: ", error.Error())
+	}
+	topics := []string{"test"}
+	consumer.SubscribeTopics(topics, nil)
+	for {
+		msg, error := consumer.ReadMessage(-1)
+		if error != nil {
+			fmt.Println("Error reading message: ", error.Error())
+		}
+		fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+	}
+}
